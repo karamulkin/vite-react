80c847c523de5853657be986ea36e291eec33f59
Deniss <karamulkin@gmail.com>
2024-05-10 15:02:46 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import pizza from "./pizza.jpeg"
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://lookool.ee" target="_blank">
+          <img 
+          src={pizza} 
+          className="pizza" 
+          alt="pizza"
+          style={{ maxWidth: 400, width: 300}}
+          />
+        </a>
+      </div>
+      <h1>Some pizza</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          счётчик {count}
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Как понравилась пицца?
+      </p>
+    </>
+  )
+}
+
+export default App
